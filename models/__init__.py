ca2449f48b2e692b795e2ec1aeb71970804d15c0
Ni1cky <nikmbogdanov@gmail.com>
2023-05-24 23:01:37 +0300

    **Everything** work

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 from .user import User
 from .subtask import Subtask
 from .tasks_groups import tasks_groups_table
-from .group import Group
 from .task import Task
+from .group import Group
 from .deadline import Deadline
